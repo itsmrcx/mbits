b2d43fcb539b8838963857874257539b7c0cdcca
itsmrcx <peekmyip@gmail.com>
2025-12-24 20:33:35 +0200

    Update Logo.tsx

--- src/assets/Logo.tsx
+++ src/assets/Logo.tsx
@@ -3,13 +3,14 @@ import React from 'react';
 export const Logo = (props: React.ImgHTMLAttributes<HTMLImageElement>) => {
   return (
     <img
-      src="/mbits/pwa-192x192.png"
-      alt="mBits Logo"
+      src="/mbits/pwa-192x192.png" 
+      alt="mBit Logo"
       {...props}
       style={{
         height: '40px',
         width: 'auto',
-        borderRadius: '0px', // Enforce square edges
+        borderRadius: '0px', 
+        border: '1px solid #00FF41', /* Adds a matrix border to the logo */
         ...props.style
       }}
     />
